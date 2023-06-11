bf468bed2828780b2b2ec7a053c8a7fa3a5938a7
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-06-11 21:23:01 +0600

    updated

--- lens-xpert-client/src/pages/Dashboard/DashboardHome.jsx
+++ lens-xpert-client/src/pages/Dashboard/DashboardHome.jsx
@@ -1,13 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Helmet } from "react-helmet";
+import { AuthContext } from '../../provider/AuthProvider';
+import axios from 'axios';
 
 const DashboardHome = () => {
+    const { user } = useContext(AuthContext)
+
+    // Getting this from Firebase;
+    const value = user.metadata.createdAt;
+    const referenceValue = 10000000000000;
+    const percentage = (value / referenceValue) * 100;
+
+
+    const [totalClass, setTotalClass] = useState('')
+    const [totalStudents, setTotalStudents] = useState('')
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_SERVER_API}/totalProducts`)
+            .then(res => {
+                setTotalClass(res.data);
+            })
+
+        // Total students
+        axios.get(`${import.meta.env.VITE_SERVER_API}/totalStudents`)
+            .then(res => {
+                setTotalStudents(res.data);
+            })
+
+    }, [])
     return (
-        <div>
+        <div className='mb-12'>
             <Helmet>
                 <title>Dashboard | LensXpert</title>
             </Helmet>
-            <h3 className='text-3xl text-center pt-5'>Welcome to the Dashboard. Manage your activity..</h3>
+            <h3 className='text-3xl text-center py-5'>Welcome to the Dashboard. Manage your activity..</h3>
+            <hr />
+            {/* Stylish Dashboard */}
+
+            <div className='flex justify-center items-center my-5'>
+                <div className=" md:stats shadow">
+
+                    <div className="stat">
+                        <div className="stat-figure text-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                        </div>
+                        <div className="stat-title text-slate-600">Total Students</div>
+                        <div className="stat-value text-primary">{totalStudents.totalStudents}</div>
+                        <div className="stat-desc text-slate-600">Student is increasing..</div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                        </div>
+                        <div className="stat-title text-slate-600">Total Class</div>
+                        <div className="stat-value text-secondary">{totalClass.totalProducts}</div>
+                        <div className="stat-desc text-slate-600">Classes are helpful.</div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <div className="avatar online">
+                                <div className="w-16 rounded-full">
+                                    <img src={user?.photoURL} title={user?.displayName} alt='SmMunna'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="stat-value">{percentage.toFixed(2) + '%'}</div>
+                        <div className="stat-title text-slate-600">{user?.displayName}</div>
+                        <div className="stat-desc text-secondary">{user?.email}</div>
+                    </div>
+
+                </div>
+            </div>
+
+
         </div>
     );
 }
--- lens-xpert-client/src/pages/EnrolledCourse/EnrolledCourse.jsx
+++ lens-xpert-client/src/pages/EnrolledCourse/EnrolledCourse.jsx
@@ -5,7 +5,7 @@ import { Helmet } from 'react-helmet';
 
 const EnrolledCourse = () => {
     const [userpayment, setUserpayment] = useState([])
-    const[itemId,setItemid]=useState([])
+    const [itemId, setItemid] = useState([])
     const { user } = useContext(AuthContext)
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_SERVER_API}/userpaymenthistory?email=${user?.email}`)
@@ -18,14 +18,14 @@ const EnrolledCourse = () => {
     }, [])
     return (
         <div className='px-12 md:px-24'>
-        <Helmet>
-            <title>Enrolled Class | LensXpert</title>
-        </Helmet>
+            <Helmet>
+                <title>Enrolled Class | LensXpert</title>
+            </Helmet>
             <h3 className='text-center py-3 text-2xl font-semibold'>Enrolled Classes</h3>
             <hr />
             <div className='flex justify-center py-4'>
                 <div>
-                    <ul className="menu bg-orange-950 text-white  w-full rounded-box">
+                    <ul className="menu bg-orange-950 text-white rounded-box ">
                         <li>
                             <h2 className=" text-white font-semibold">Photography Classes</h2>
                             <hr />
@@ -33,9 +33,9 @@ const EnrolledCourse = () => {
                                 {
                                     userpayment.map((paymentItem, index) => <React.Fragment key={paymentItem._id}>
                                         {
-                                            paymentItem.itemname.map((myitem, index) => <React.Fragment key={index+1}>
+                                            paymentItem.itemname.map((myitem, index) => <React.Fragment key={index + 1}>
 
-                                                <li><>🤴 {myitem}</></li>
+                                                <li className='mr-5'><>🤴 {myitem}</></li>
                                             </React.Fragment>)
                                         }
                                     </React.Fragment>)
